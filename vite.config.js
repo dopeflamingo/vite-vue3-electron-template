36a46b08101a8fa625aa3b6da20c96f21ae2c96e
Dope Fla <zizek21@gmail.com>
2021-06-06 02:20:35 +0900

    == Fixed Bug ==
    * Modified vite.config.js to support vite preview and electron both well

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ const path = require("path")
 export default ({ command, mode }) => {
   if (command === 'build') {
     return {
-      base: path.resolve(__dirname, "./dist/"),
+      // base: path.resolve(__dirname, "dist/"),
+      base: './',
       plugins: [vue()],
+      build: {
+        outDir: path.join(__dirname, 'dist'),
+        emptyOutDir: true,
+        minify: false,
+        commonjsOptions: {},
+        assetsDir: '', 
+        sourcemap: true,
+      },
 
     }
   } else {
